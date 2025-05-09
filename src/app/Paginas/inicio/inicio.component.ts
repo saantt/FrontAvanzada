dcfc17f48cef1587a11244d89005888b55be051b
saantt <santiagomaring05@gmail.com>
2025-05-09 12:56:55 -0500

    todo el front listo

--- src/app/Paginas/inicio/inicio.component.ts
+++ src/app/Paginas/inicio/inicio.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { LoginComponent } from '../login/login.component';
+import { RegistroComponent } from '../registro/registro.component';
 @Component({
   selector: 'app-inicio',
   imports: [],
@@ -7,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
+  constructor(private dialog: MatDialog) {}
 
+  abrirLogin(): void {
+    this.dialog.open(LoginComponent, {
+      width: '90%',
+      maxWidth: '400px'
+    });
+  }
+
+  abrirRegistro(): void {
+    this.dialog.open(RegistroComponent, {
+      width: '90%',
+      maxWidth: '400px'
+    });
+  }
 }
